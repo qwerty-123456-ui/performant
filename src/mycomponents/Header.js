f7375ced1ca6456e3761baef3dc6ebfbf9e4a171
qwerty935 <isha26701@gmail.com>
2021-05-22 00:55:46 +0530

    first commit

--- src/mycomponents/Header.js
+++ src/mycomponents/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import Nav from 'react-bootstrap/Nav';
 // import FormControl from 'react-bootstrap/FormControl';
 // import Form from 'react-bootstrap/Form';
 // import Button from 'react-bootstrap/Button';
@@ -10,14 +10,14 @@ function Header() {
   return (
     <div>
       <>
-        <Navbar bg="dark" variant="dark">
+        {/* <Navbar bg="dark" variant="dark">
           <Navbar.Brand href="#home">Performant</Navbar.Brand>
           <Nav className="mr-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href="/contact">Contact</Nav.Link>
           </Nav>
-        </Navbar>
+        </Navbar> */}
         {/* <br /> */}
       </>
     </div>
--- src/mycomponents/Home.js
+++ src/mycomponents/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Alert from "react-bootstrap/Alert";
+// import Alert from "react-bootstrap/Alert";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Carousel from "react-bootstrap/Carousel";
@@ -11,35 +11,35 @@ import Badge from 'react-bootstrap/Badge'
 
 
 
-function AlertDismissible() {
-  const [show, setShow] = useState(true);
+// function AlertDismissible() {
+//   const [show, setShow] = useState(true);
 
-  return (
-    <>
-      <Alert show={show} variant="success">
-        <Alert.Heading>How's it going?!</Alert.Heading>
-        <p>Welcome to PERFORMANT!</p>
-        <Button variant="dark">
-          Profile <Badge variant="light">1M+</Badge>
-          <span className="sr-only">unread messages</span>
-        </Button>
-        <hr />
-        <div className="d-flex justify-content-end">
-          <Button onClick={() => setShow(false)} variant="outline-success">
-            Close me y'all!
-          </Button>
-        </div>
-      </Alert>
+//   return (
+//     <>
+//       <Alert show={show} variant="success">
+//         <Alert.Heading>How's it going?!</Alert.Heading>
+//         <p>Welcome to PERFORMANT!</p>
+//         <Button variant="dark">
+//           Profile <Badge variant="light">1M+</Badge>
+//           <span className="sr-only">unread messages</span>
+//         </Button>
+//         <hr />
+//         <div className="d-flex justify-content-end">
+//           <Button onClick={() => setShow(false)} variant="outline-success">
+//             Close me y'all!
+//           </Button>
+//         </div>
+//       </Alert>
 
-      {!show && <Button variant="success" onClick={() => setShow(true)}>Show Alert</Button>}
-    </>
-  );
-}
+//       {!show && <Button variant="success" onClick={() => setShow(true)}>Show Alert</Button>}
+//     </>
+//   );
+// }
 
 function Home() {
   return (
     <>
-      <AlertDismissible />
+      {/* <AlertDismissible /> */}
       <h1 style={{fontSize: "100px", color: "rgb(15, 131, 146)"}}>PERFORMANT</h1>
       <video src={video1} width="100%" height="700" loop autoplay="true" />
       <br></br>
